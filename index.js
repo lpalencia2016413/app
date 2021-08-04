24db9250279e88a4a04dc80d04529b3aea788a9f
lpalencia2016413 <69654072+lpalencia2016413@users.noreply.github.com>
2021-08-03 21:53:42 -0600

    Cambios hechos

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = require("./app")
 var usuarioControlador = require("./src/controladores/usuarios.controlador");
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/Eathly', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect('mongodb+srv://administrador:cbf7d8dddb@eathly.3ji8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log('Se encuentra conectado a la base de datos');
-    app.listen(3000, function () {
+    app.listen(process.env.Port || 3000, function () {
         console.log("Servidor corriendo en el puerto 3000");
         usuarioControlador.admin();
     })
